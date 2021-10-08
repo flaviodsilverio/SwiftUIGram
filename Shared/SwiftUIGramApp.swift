824baa6db0efa6655e8ca8b4f1739e9173ef75f0
Flavio Silverio <flaviodsilverio@gmail.com>
2021-10-08 11:39:18 +0200

    Initial Commit

---
+++ Shared/SwiftUIGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGramApp.swift
+//  Shared
+//
+//  Created by Flávio Silvério on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
