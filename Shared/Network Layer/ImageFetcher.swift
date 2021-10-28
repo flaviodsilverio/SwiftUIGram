de6db3807c29d53d4bab8ec49903e70931b29d03
Flavio Silverio <flaviodsilverio@gmail.com>
2021-10-28 17:37:02 +0200

    refinements to the network layer

--- Shared/Network Layer/ImageFetcher.swift
+++ Shared/Network Layer/ImageFetcher.swift
@@ -15,15 +15,19 @@ enum NetworkError: Error {
 }
 
 public class ImageFetcher {
-    let requestMaker = RequestMaker()
+    let requestClient: RequestClient
     var cancellables: [AnyCancellable] = []
     
-    func fetchImage(for urlString: String, completion: @escaping (UIImage?) -> ()) {
-        requestMaker.make(requestWithURLString: urlString)
-            .sink { receiveCompletion in
-                print("completion")
+    init(_ requestClient: RequestClient = RequestClient()) {
+        self.requestClient = requestClient
+    }
+    
+    func fetchImage(for urlString: String,completion: @escaping (Result<UIImage?, Error>) -> ()) {
+        requestClient.make(requestWith: urlString)
+            .sink { _ in
+                
             } receiveValue: { data in
-                completion(ImagerParser.parse(data))
+                completion(.success(ImagerParser.parse(data)))
             }.store(in: &cancellables)
     }
 }
@@ -34,66 +38,16 @@ public class ImagerParser {
     }
 }
 
-public class RequestMaker {
-    let session: URLSession
-
-    init(_ session: URLSession = URLSession.shared) {
-        self.session = session
-    }
-    
-    func make(requestWithURLString string: String) -> AnyPublisher <Data, Error> {
-        return session.dataTaskPublisher(for: URLRequest(url: URL(string: string)!)).tryMap { data, response in
-            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
-                throw APIError.unknown
-            }
-            return data
-
-        }
-        .mapError { error in
-            if let error = error as? APIError {
-                return error
-            } else {
-                return APIError.apiError(description: error.localizedDescription)
-            }
-        }
-        .receive(on: DispatchQueue.main)
-        .eraseToAnyPublisher()
-    }
-    
-    func make(requestWithURLString string: String, completion: @escaping (_ data: Data?, _ error: NetworkError?)->()) {
-        
-        guard let url = URL(string: string) else {
-            completion(nil, NetworkError.malformedURL)
-            return
-        }
-                
-        session.dataTask(with: URLRequest(url: url)) { data, response, error in
-            guard let response = response as? HTTPURLResponse,
-                  response.statusCode == 200,
-                  let data = data,
-                  error == nil else {
-                completion(nil, NetworkError.unknown)
-                return
-            }
-            
-            completion(data, nil)
-            
-        }.resume()
-    }
-}
-
-
 protocol Requestable {
     func make(
-        _ request: URLRequest,
-        _ decoder: JSONDecoder
+        requestWith urlString: String
     ) -> AnyPublisher<Data, Error>
 }
 
-struct RequestClient: Requestable {
-    func make(_ request: URLRequest, _ decoder: JSONDecoder) -> AnyPublisher <Data, Error> {
+final class RequestClient: Requestable {
+    func make(requestWith urlString: String) -> AnyPublisher <Data, Error> {
         return URLSession
-            .DataTaskPublisher(request: request, session: .shared)
+            .DataTaskPublisher(request: URLRequest(url: URL(string: urlString)!), session: .shared)
             .tryMap { data, response in
                 guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
                     throw APIError.unknown
--- Shared/View Models/PostViewModel.swift
+++ Shared/View Models/PostViewModel.swift
@@ -9,35 +9,40 @@
 /// There should be more View Models for the components, however since the goal of this project is to explore SwiftUI, this will be used mostly everywhere.
 import SwiftUI
 
-class PostViewModel: ObservableObject {
+final class PostViewModel: ObservableObject {
     @Published var item: FeedItem
     @Published var image: UIImage?
     @Published var profileImage: UIImage?
     
-    let imageFetcher = ImageFetcher()
-    
-    init(with item: FeedItem) {
+    private let imageFetcher: ImageFetcher
+
+    init(with item: FeedItem, _ imageFetcher: ImageFetcher = ImageFetcher()) {
         self.item = item
+        self.imageFetcher = imageFetcher
     }
     
     func fetchImage() {
-        imageFetcher.fetchImage(for: item.image.imageURL) { [weak self] image in
-            guard let image = image else {
-                return
-            }
-            DispatchQueue.main.async {
-                self?.image = image
+        imageFetcher.fetchImage(for: item.image.imageURL) { [weak self] result in
+            switch result {
+            case .success(let image):
+                DispatchQueue.main.async {
+                    self?.image = image
+                }
+            case .failure(let error):
+                debugPrint(error)
             }
         }
     }
     
     func fetchProfileImage() {
-        imageFetcher.fetchImage(for: item.author.profileImgURL) { [weak self] image in
-            guard let image = image else {
-                return
-            }
-            DispatchQueue.main.async {
-                self?.profileImage = image
+        imageFetcher.fetchImage(for: item.author.profileImgURL) { [weak self] result in
+            switch result {
+            case .success(let image):
+                DispatchQueue.main.async {
+                    self?.profileImage = image
+                }
+            case .failure(let error):
+                debugPrint(error)
             }
         }
     }
