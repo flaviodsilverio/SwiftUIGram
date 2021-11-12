13e8d7453d191e8ea4ee9b21acd98e16ddc52bc0
flaviodsilverio <flaviodsilverio@gmail.com>
2021-11-12 09:12:04 +0100

    Installs and configures the Parse connection

---
+++ iOS/Constants/Constants.swift
@@ -0,0 +1,25 @@
+//
+//  Constants.swift
+//  Constants
+//
+//  Created by Flávio Silvério on 07/11/2021.
+//
+
+import Foundation
+
+enum ImageNames {
+    enum Interactions: String {
+        case bookmark = "folder.badge.plus"
+        case bookmarkFilled = "folder.fill.badge.minus"
+        case like = "heart"
+        case likeFilled = "heart.fill"
+        case send = "paperplane"
+
+    }
+}
+
+enum ParseConfiguration {
+    static let appID = "ODfaFTVLIyck97FPykZVzHxT85chAujs3DUTIB3t"
+    static let appKey = "305dKjrtpCMZHh1Hi9cg2Ta93Rh8hDYStmKaD4S3"
+    static let databaseURL = "https://parseapi.back4app.com"
+}
